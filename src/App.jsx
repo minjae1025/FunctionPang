5f4aa4cc9e140cc1bff486c542b4f164bfec5807
minjae1025 <kjt081025@gmail.com>
2026-06-09 10:42:02 +0900

    Github Pages 최적화

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import { Routes, Route } from 'react-router-dom'
-import './App.css'
-import '@/styles/reset.css'
-import Index from '@/routes/Index'
-import Mypage from '@/routes/Mypage'
-import Home from '@/routes/Home'
+import { useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import './App.css';
+import '@/styles/reset.css';
+import Index from '@/routes/Index';
+import Mypage from '@/routes/Mypage';
+import Home from '@/routes/Home';
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // 세션 스토리지에서 리다이렉트할 경로가 있는지 확인
+    const redirect = sessionStorage.redirect;
+    if (redirect) {
+      delete sessionStorage.redirect; // 사용 후 삭제
+
+      // 베이스 경로 제거 ('/FunctionPang')
+      const path = redirect.replace('/FunctionPang', '');
+
+      navigate(path);
+    }
+  }, [navigate]);
 
   return (
     <Routes>
@@ -13,7 +28,7 @@ function App() {
       <Route path="/home/*" element={<Home />} />
       <Route path="/*" element={<Index />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
